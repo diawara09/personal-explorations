b56ad0f609b34b45bd32d1dca330885c3563d135
solitdio079 <solitdio079@gmail.com>
2025-04-27 20:42:38 +0300

    Add access token to request

--- server/routes/tokens.mjs
+++ server/routes/tokens.mjs
@@ -1,10 +1,47 @@
 import express,{Router} from 'express'
 import Token from '../models/tokens.mjs'
 
-
+import {google} from 'googleapis'
+import serviceAccount from '../privatekeys.json' with { type: "json" };
 const router = Router()
 
 router.use(express.json())
+
+
+
+
+
+
+// Define the required scopes.
+var scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/firebase.database"
+];
+
+// Authenticate a JWT client with the service account.
+var jwtClient = new google.auth.JWT(
+  serviceAccount.client_email,
+  null,
+  serviceAccount.private_key,
+  scopes
+);
+let accessToken
+
+// Use the JWT client to generate an access token.
+jwtClient.authorize(function(error, tokens) {
+  if (error) {
+    console.log("Error making request to generate access token:", error);
+  } else if (tokens.access_token === null) {
+    console.log("Provided service account does not have permission to generate access tokens");
+  } else {
+     accessToken = tokens.access_token;
+
+    // See the "Using the access token" section below for information
+    // on how to use the access token to send authenticated requests to
+    // the Realtime Database REST API.
+  }
+});
+
 router.post("/", async(req,res) => {
     const {token} = req.body
 
@@ -39,8 +76,9 @@ router.get("/", async(req,res) => {
             mode:"cors",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": "bearer AIzaSyAzhoCSeWRGb2OBbXZM9Bzc9orgamZdeu0"
+                Authorization: `Bearer ${accessToken} `
             },
+            
             body: JSON.stringify(data)
         })
         const response = await sendNotification.json()
