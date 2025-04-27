a6bb397ecc7d17b82fc3060e8b0c117a5e5882d0
solitdio079 <solitdio079@gmail.com>
2025-04-27 19:18:36 +0300

    Add namespaces API

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,18 +1,29 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
-import firebaseConfig from '../utils/firebaseApp'
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp(firebaseConfig);
+firebase.initializeApp({
+    apiKey: "AIzaSyAzhoCSeWRGb2OBbXZM9Bzc9orgamZdeu0",
+    authDomain: "realtime-database-85a38.firebaseapp.com",
+    databaseURL: "https://realtime-database-85a38-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "realtime-database-85a38",
+    storageBucket: "realtime-database-85a38.firebasestorage.app",
+    messagingSenderId: "491623270168",
+    appId: "1:491623270168:web:15b174fd9f931a2df6006d"
+});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = getMessaging(firebaseApp);
+const messaging = firebase.messaging();
 
-onBackgroundMessage(messaging, (payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+      '[firebase-messaging-sw.js] Received background message ',
+      payload
+    );
     // Customize notification here
     const notificationTitle = 'Background Message Title';
     const notificationOptions = {
@@ -20,6 +31,5 @@ onBackgroundMessage(messaging, (payload) => {
       icon: '/firebase-logo.png'
     };
   
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
-});
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
