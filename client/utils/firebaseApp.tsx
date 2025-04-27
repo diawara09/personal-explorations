ee614fa49c7dccf03c7c50d75a2a2b40bea5abde
solitdio079 <solitdio079@gmail.com>
2025-04-27 15:22:57 +0300

    Add request permission to welcome

---
+++ client/utils/firebaseApp.tsx
@@ -0,0 +1,16 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzhoCSeWRGb2OBbXZM9Bzc9orgamZdeu0",
+  authDomain: "realtime-database-85a38.firebaseapp.com",
+  databaseURL: "https://realtime-database-85a38-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "realtime-database-85a38",
+  storageBucket: "realtime-database-85a38.firebasestorage.app",
+  messagingSenderId: "491623270168",
+  appId: "1:491623270168:web:15b174fd9f931a2df6006d"
+};
+
+
+
+
+
+export default firebaseConfig
