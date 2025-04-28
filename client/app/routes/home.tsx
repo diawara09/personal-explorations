0e591af0323e5d11c19b5672aafcf0cded661724
solitdio079 <solitdio079@gmail.com>
2025-04-28 15:48:28 +0300

    Put the loader in home.tsx

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,11 +1,31 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { serverUrl } from "utils/serverUrl";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
+}
+
+export async function clientLoader(){
+  try {
+    const req = await fetch(serverUrl + "/token/", {
+      method: "GET",
+      mode:"cors",
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    const response = await req.json()
+    console.log(response);
+    return response
+    
+  } catch (error) {
+    return {error}
+  }
+  
 }
 
 export default function Home() {
--- client/app/welcome/welcome.tsx
+++ client/app/welcome/welcome.tsx
@@ -6,24 +6,7 @@ import { useEffect, useState } from "react";
 import firebaseConfig from "utils/firebaseApp";
 import { serverUrl } from "utils/serverUrl";
 
-export async function clientLoader(){
-  try {
-    const req = await fetch(serverUrl + "/token/", {
-      method: "GET",
-      mode:"cors",
-      headers: {
-        'Content-Type':'application/json'
-      }
-    })
-    const response = await req.json()
-    console.log(response);
-    return response
-    
-  } catch (error) {
-    return {error}
-  }
-  
-}
+
 
 
 export function Welcome() {// Initialize Firebase
