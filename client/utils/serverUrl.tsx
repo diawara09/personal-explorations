e80e89f4cdbc460f9e947995ef4a0bbdeb5fe80f
diawara09 <diawaraholding223@gmail.com>
2025-04-30 19:44:46 +0300

    Switch to development server

--- client/utils/serverUrl.tsx
+++ client/utils/serverUrl.tsx
@@ -1,1 +1,2 @@
-export const serverUrl = "https://personal-explorations.onrender.com"
+export const serverUrl = "http://localhost:300"
+//export const serverUrl = "https://personal-explorations.onrender.com"
