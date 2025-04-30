f2dbf5e7037a3ae3f756136f9492ac63c1664631
diawara09 <diawaraholding223@gmail.com>
2025-04-30 21:38:05 +0300

    Center the login card

--- client/app/routes/login.tsx
+++ client/app/routes/login.tsx
@@ -3,16 +3,16 @@ import toast, {Toaster} from 'react-hot-toast'
 export default function Login(){
     const fetcher = useFetcher()
     return (<>
-     <div className="hero bg-base-200 min-h-screen">
+     <div className="hero bg-base-200 min-h-screen ">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card bg-base-100 w-full mx-auto my-5 max-w-60 shrink-0 shadow-2xl rounded-none">
           <div className="card-body">
             <fetcher.Form
               method="post"
               className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box"
             >
               <legend className="fieldset-legend">Login</legend>
-              <label className="fieldset-label">Email</label>
+             
               <input
                 type="email"
                 name="email"
